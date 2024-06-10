4d800c114a658d573d69fcfbb7b4e4a2d0440ba4
Dmitry Kudrin <d.kudrin@growfood.pro>
2024-06-10 17:14:09 +0200

    Project init

---
+++ LKTest/LKTest/LKTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LKTestApp.swift
+//  LKTest
+//
+//  Created by Dmitriy Kudrin on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct LKTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
